bef375207accf4d6e5b9d62eaa84af4a6cf6139f
Haitham <hmmelsm@gmail.com>
2026-06-08 13:11:58 +0300

    docs: add sandbox type definitions

---
+++ docs/sandbox.d.lua
@@ -0,0 +1,15 @@
+---@meta
+
+---A sandbox repository to test BlueLua CI/CD workflow.
+---@class BlueluaSandbox
+local M = {
+  _VERSION = "sandbox 0.0.0", -- x-release-please-version
+}
+
+---Adds two numbers together.
+---@param a number The first number.
+---@param b number The second number.
+---@return number The sum of a and b.
+function M.add(a, b) end
+
+return M
